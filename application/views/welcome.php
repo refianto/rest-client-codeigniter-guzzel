5db479f5a7e7510b6f1e1bad1bd0607163f66aa3
refiantoyusuf <refiantoyusuf97@gmail.com>
2019-07-22 14:34:26 +0700

    edit index

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,1 +1,12 @@
-<h1>Welcome</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div style="text-align: center;">
+		<h1>It's Me?</h1>
+		<img src="/assets/welcome.jpg" width="25%" height="25%">	
+	</div>
+</body>
+</html>
